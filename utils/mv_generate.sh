dd80dfe87790675502abdfa5eabdc76790116749
Frank <0979030@gmail.com>
2025-04-07 16:30:37 +0800

    fix: submodule push

--- utils/mv_generate.sh
+++ utils/mv_generate.sh
@@ -3,11 +3,11 @@
 
 IMAGE_PATH="assets/demo/i2mv/A_decorative_figurine_of_a_young_anime-style_girl.png"
 TEXT="A decorative figurine of a young anime-style girl"
-OUTPUT_DIR="../result/test_i2mv_sdxl/test.png"
+OUTPUT_PATH="../result/test_i2mv_sdxl/test.png"
 
 cd ../MV-Generation
 python -m scripts.inference_i2mv_sdxl \
 --image "$IMAGE_PATH" \
 --text "$TEXT" \
 --seed 21  \
---output "$OUTPUT_DIR" --remove_bg
+--output "$OUTPUT_PATH" --remove_bg
